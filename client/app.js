a544af28a8971a06db9f6887ca7e947543298428
jdlubrano <joel.lubrano@gmail.com>
2016-03-26 20:55:34 -0400

    Build one wedge

--- client/app.js
+++ client/app.js
@@ -22,8 +22,9 @@ function initWheel() {
   var svg = d3.select('.wheel');
   var svgWidth = 350;
   var svgHeight = 350;
+  var padding = 10;
 
-  var radius = svgWidth / 2 - 10;
+  var radius = svgWidth / 2 - padding;
   var cx = svgWidth / 2;
   var cy = svgHeight / 2;
 
@@ -33,19 +34,47 @@ function initWheel() {
     .attr('cy', cy)
   ;
 
-  var wedgeGroup = svg.append('g');
+  var allWedges = svg.append('g');
 
-  var wedge = wedgeGroup.append('path')
-    .attr('class', 'wedge')
+  var wedge = allWedges.append('g').attr('class', 'wedge');
+
+  function drawWedge() {
+   return 'M' + [cx,cy].join(',') + 
+    ' L' + [cx, cy - radius].join(',') +
+    ' A' + [radius, radius].join(',') +
+    ' 0 0, 1 ' + [cx + radius, cy].join(',') +
+    ' z';
+  }
+
+  function rotateWedge(wedge, i) {
+    return 'rotate(' + [90 * i - 45, cx, cy].join(',') + ')';
+  }
+
+  var colorScale = d3.scale.category10();
+
+  wedge.append('path')
+    .attr('d', drawWedge)
+    .attr('fill', colorScale(0))
   ;
-  debugger;
 
-  wedge.attr('d',
-             'M' + [cx,cy].join(',') + 
-              ' L' + [cx + r, cy].join(',') +
-              ' A' + [radius, radius].join(',') +
-              ' 0 0, 1 ' + [cx, cy + r].join(',') +
-              ' z'
-            );
+  var text = wedge.append('text')
+    .attr('class', 'facial-hair-style vertical')
+    .attr('x', cx)
+    .attr('y', 30)
+    .attr('transform', function(d, i) {
+      return 'rotate(' + [45, cx, cy].join(',') + ')';
+    });
+  ;
+
+  for(var i = 0; i < 'Mustache'.length; i++) {
+    text.append('tspan')
+      .attr('rotate', '-90')
+      .text('Mustache'[i])
+    ;
+  }
+
+  var wedges = allWedges.selectAll('.wedge')
+    .attr('transform', rotateWedge)
+  ;
 }
 
